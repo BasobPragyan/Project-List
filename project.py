3634a6d59cd5e7e5e77fe568d403533866e4feb7
BasobPragyan <pragyan95prabodhbanerjee@gmail.com>
2025-02-04 20:57:50 +0530

    Project List

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 li=int(input("Enter your list of odd numbers : "))
 list1=[li]
 list2=[11,13,15,17,19,21,23,25]
-result=map(lambda x,y:x+y,list1,list2)
+result=map(lambda x,y:x**y,list1,list2)
 print("First list is : ",list1)
 print("Second list is : ",list2)
-print("Final add on both lists is : ",list(result))
+print("Final squaring on both lists is : ",list(result))
